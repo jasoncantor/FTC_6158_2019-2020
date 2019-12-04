e3ca2526792a083640ad5cf6563b382d10c39e28
jasoncantor <46542133+jasoncantor@users.noreply.github.com>
2019-12-04 08:59:05 -0500

    Updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name = "6158 2019-2020 Auto", group="Final")
+@Autonomous(name = "Depo Left: 6158 2019-2020 Auto", group="Final")
 public class Auto extends LinearOpMode {
     //
     DcMotor frontleft;
